ecb9c551f6aaf443b0b66fc316cec6b0c5d96e91
gianmillare <gianmillaregsm@gmail.com>
2020-03-22 20:43:21 -0700

    started the models.py file with a text and datetime attribute

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Topic(models.Model): # This is where we create the backbone. 'Model' is included with Django for model's functionality
+    """ Topic the User is learning """
+
+    text = models.CharField(max_length=200) # this attribure 'text' takes in characters via 'CharField'
+    date_added = models.DateTimeField(auto_now_add=True) # this attribute passes the current data and time
+
+    def __str__(self): # this will return a string attribute inputted from the text attribute
+        """ Return a string representation of the model """
+        return self.text
