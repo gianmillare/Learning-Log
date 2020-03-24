2f68fc555204ee6384281b95dfa820aa3a6c2668
gianmillare <gianmillaregsm@gmail.com>
2020-03-23 19:07:25 -0700

    created the urls.py file for all url patterns and paths

---
+++ learning_logs/urls.py
@@ -0,0 +1,13 @@
+""" Define URL Patterns for learning_logs """
+
+from django.urls import path # dependency
+from . import  views # importing the views module from the same directory as this file
+
+app_name = 'learning_logs'
+urlpatterns = [
+    # This is where you would put all the paths for the app
+    # Home page;
+    path('', views.index, name='index'), # The first input is an empty string because it refers to the end of the URL
+    # Because the home page url is 'http://127.0.0.1:8000/' there is no string at the end. Hence, it is empty.
+    # Other URLs in this page will have a pattern at the end of the above URL
+]
