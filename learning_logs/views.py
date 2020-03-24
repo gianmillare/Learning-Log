76af8732a1bc23eb9e70b13aca70206ce0317dd6
gianmillare <gianmillaregsm@gmail.com>
2020-03-23 19:19:13 -0700

    wrote a script into the views.py file to render the index page

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+# Functions below must match a path in the main app file. In this case learning_logs/urls.py; function views.index
+def index(request):
+    """ homepage for learning log. """
+    return render(request, 'learning_logs/index.html')
