1f53c54010c9ed63a2e26f245a9391ec23320f34
gianmillare <gianmillaregsm@gmail.com>
2020-03-23 20:57:59 -0700

    created the topics landing page and added the path accordingly to cooperate with the rest of the Django files

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path('', views.index, name='index'), # The first input is an empty string because it refers to the end of the URL
     # Because the home page url is 'http://127.0.0.1:8000/' there is no string at the end. Hence, it is empty.
     # Other URLs in this page will have a pattern at the end of the above URL
+
+    # Page that shows all topics
+    path('topics/', views.topics, name='topics'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
+from .models import Topic # we import this because the models.py file has data under a class called Topic
+
 # Create your views here.
 # Functions below must match a path in the main app file. In this case learning_logs/urls.py; function views.index
 def index(request):
     """ homepage for learning log. """
     return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """ Show all topics """
+    topics = Topic.objects.order_by('date_added') # Here we store all the information from the class and object 'date_added' into another variable 'topics'
+    context = {'topics': topics} # Here we define a context --> a key value pairing containing a set of topics from models.py
+    return render(request, 'learning_logs/topics.html', context) # This is a standard render that will display the context, request, and house the file path
