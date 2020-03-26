ce38aa08c4aaefe82b16f0ab58354b8ed02a18fe
gianmillare <gianmillaregsm@gmail.com>
2020-03-25 19:58:36 -0700

    created another app titled users to make other people available to make an account with the app

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # My apps: This is where you would add in your application
     'learning_logs',
+    'users',
 
     # Default django apps
     'django.contrib.admin',
