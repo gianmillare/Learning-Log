487c965c65a578660ba8eaa13c7eec605b0c5323
gianmillare <gianmillaregsm@gmail.com>
2020-03-25 18:52:36 -0700

    created the new_topic function in views.py, allowing for a form page to show

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect # Redirect will send the user back to the topics page once the form is submitted
 
 from .models import Topic # we import this because the models.py file has data under a class called Topic
+from .forms import TopicForm
 
 # Create your views here.
 # Functions below must match a path in the main app file. In this case learning_logs/urls.py; function views.index
@@ -20,3 +21,19 @@ def topic(request, topic_id): # whatever is captured in '/<int:topic_id>/' from 
     entries = topic.entry_set.order_by('-date_added') # we get the entries associated with the topic and order them by date_added. The '-' reverses the order
     context = {'topic': topic, 'entries': entries} # we store the topics and entries into context
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """ Add a new topic """
+    if request.method != 'POST':
+        # No data submitted, create a blank form
+        form = TopicForm()
+    else:
+        # POST data submitted; process data.
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+
+    # Display a blank or invalid form
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
