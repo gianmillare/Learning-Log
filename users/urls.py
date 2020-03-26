403fa4446f8d1c51a3848da175ad42a9769ff5c1
gianmillare <gianmillaregsm@gmail.com>
2020-03-25 20:31:36 -0700

    created a function for registering a new user in the views.py file in the users directory

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,14 @@
 """ Defines urls for users """
 
 from django.urls import path, include
+from . import views
+
 
 app_name = 'users'
 urlpatterns = [
     # We add in default admin urls
     path('', include('django.contrib.auth.urls')),
+
+    # Registration page.
+    path('register/', views.register, name='register'),
 ]
