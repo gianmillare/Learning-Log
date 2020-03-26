529e420242a391e36323d856747a06ab25302603
gianmillare <gianmillaregsm@gmail.com>
2020-03-25 19:45:06 -0700

    created an edit_entry function and made it passable to the applciation

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect # Redirect will send the user back to the topics page once the form is submitted
 
-from .models import Topic # we import this because the models.py file has data under a class called Topic
+from .models import Topic, Entry # we import this because the models.py file has data under a class called Topic
 from .forms import TopicForm, EntryForm
 
 # Create your views here.
@@ -57,3 +57,21 @@ def new_entry(request, topic_id):
     # Display a blank or invalid form
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
+
+def edit_entry(request, entry_id):
+    """ Edit an existing entry """
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        # if no data, use what is already in the entry
+        form = EntryForm(instance=entry)
+    else:
+        # POST data submitted; process data
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topic', topic_id=topic_id)
+
+    context = {'entry': entry, 'topic':topic, 'form': form}
+    return render(request, 'learning_logs/edit_entry.html', context)
